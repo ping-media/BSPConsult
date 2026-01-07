a804a236b0cdfd4a30d245cdb797cad93065ea6a
ping-media <wpexpertdeep@gmail.com>
2026-01-07 23:14:12 +0530

    deep desktop changes

--- src/components/landing-page/Header.js
+++ src/components/landing-page/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
   <div className="navbar-inner">
     <div className="brand">
       <a href="https://bspconsult.com/" className="brand-link">
-        <img src="img/bsplogo.svg" alt="BSP Consult" className="brand-logo" />
+        <img src="img/bsplogo.png" alt="BSP Consult" className="brand-logo" />
         <span className="brand-text">
           <strong>BSP</strong> CONSULT
         </span>
@@ -50,195 +50,23 @@ const Header = () => (
 
 <div className="features-list">
   <div className="feature-item">
-      <svg width="38" height="38" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2_405)">
-<rect x="8" y="6" width="31" height="31" rx="15.5" fill="white" fillOpacity="0.14" shapeRendering="crispEdges"/>
-<rect x="8.5" y="6.5" width="30" height="30" rx="15" stroke="url(#paint0_linear_2_405)" shapeRendering="crispEdges"/>
-<g clipPath="url(#clip0_2_405)">
-<g clipPath="url(#clip1_2_405)">
-<g clipPath="url(#clip2_2_405)">
-<mask id="mask0_2_405" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="16" y="14" width="15" height="15">
-<path d="M16 14H31V29H16V14Z" fill="#fff"/>
-</mask>
-<g mask="url(#mask0_2_405)">
-<path d="M29.75 20.9251V21.5001C29.7494 22.8478 29.3132 24.1593 28.5063 25.2389C27.6995 26.3184 26.5653 27.1083 25.2729 27.4906C23.9805 27.8729 22.5991 27.8272 21.3348 27.3603C20.0705 26.8934 18.991 26.0303 18.2573 24.8998C17.5236 23.7692 17.175 22.4318 17.2635 21.0869C17.352 19.7421 17.8729 18.4618 18.7484 17.4372C19.6239 16.4125 20.8072 15.6984 22.1218 15.4012C23.4364 15.104 24.8118 15.2397 26.043 15.7881" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M29.75 16.5011L26.625 19.6291L25.0625 21.1931L23.5 22.7571L21.625 20.8821" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-</g>
-</g>
-</g>
-</g>
-<defs>
-<filter id="filter0_d_2_405" x="0" y="0" width="47" height="47" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="2"/>
-<feGaussianBlur stdDeviation="4"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_405"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_405" result="shape"/>
-</filter>
-<linearGradient id="paint0_linear_2_405" x1="23.5" y1="6" x2="23.5" y2="37" gradientUnits="userSpaceOnUse">
-<stop stopColor="white" stopOpacity="0.3"/>
-<stop offset="1" stopColor="white" stopOpacity="0"/>
-</linearGradient>
-<clipPath id="clip0_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip1_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip2_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-</defs>
-      </svg>
+      <img src="img/Icon-circle.png" alt="Years" className="feature-icon" />
       <span>4+ Years Proven Track Record</span>
   </div>
 
   <div className="feature-item">
-    <svg width="38" height="38" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2_405)">
-<rect x="8" y="6" width="31" height="31" rx="15.5" fill="white" fillOpacity="0.14" shapeRendering="crispEdges"/>
-<rect x="8.5" y="6.5" width="30" height="30" rx="15" stroke="url(#paint0_linear_2_405)" shapeRendering="crispEdges"/>
-<g clipPath="url(#clip0_2_405)">
-<g clipPath="url(#clip1_2_405)">
-<g clipPath="url(#clip2_2_405)">
-<mask id="mask0_2_405" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="16" y="14" width="15" height="15">
-<path d="M16 14H31V29H16V14Z" fill="#fff"/>
-</mask>
-<g mask="url(#mask0_2_405)">
-<path d="M29.75 20.9251V21.5001C29.7494 22.8478 29.3132 24.1593 28.5063 25.2389C27.6995 26.3184 26.5653 27.1083 25.2729 27.4906C23.9805 27.8729 22.5991 27.8272 21.3348 27.3603C20.0705 26.8934 18.991 26.0303 18.2573 24.8998C17.5236 23.7692 17.175 22.4318 17.2635 21.0869C17.352 19.7381 17.8729 18.4618 18.7484 17.4372C19.6239 16.4125 20.8072 15.6984 22.1218 15.4012C23.4364 15.104 24.8118 15.2397 26.043 15.7881" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M29.75 16.5011L26.625 19.6291L25.0625 21.1931L23.5 22.7571L21.625 20.8821" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-</g>
-</g>
-</g>
-</g>
-<defs>
-<filter id="filter0_d_2_405" x="0" y="0" width="47" height="47" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="2"/>
-<feGaussianBlur stdDeviation="4"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_405"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_405" result="shape"/>
-</filter>
-<linearGradient id="paint0_linear_2_405" x1="23.5" y1="6" x2="23.5" y2="37" gradientUnits="userSpaceOnUse">
-<stop stopColor="white" stopOpacity="0.3"/>
-<stop offset="1" stopColor="white" stopOpacity="0"/>
-</linearGradient>
-<clipPath id="clip0_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip1_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip2_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-</defs>
-</svg>
+  <img src="img/Icon-circle.png" alt="Years" className="feature-icon" />
 
     <span>Single Sport Focus</span>
   </div>
 
   <div className="feature-item">
-     <svg width="38" height="38" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2_405)">
-<rect x="8" y="6" width="31" height="31" rx="15.5" fill="white" fillOpacity="0.14" shapeRendering="crispEdges"/>
-<rect x="8.5" y="6.5" width="30" height="30" rx="15" stroke="url(#paint0_linear_2_405)" shapeRendering="crispEdges"/>
-<g clipPath="url(#clip0_2_405)">
-<g clipPath="url(#clip1_2_405)">
-<g clipPath="url(#clip2_2_405)">
-<mask id="mask0_2_405" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="16" y="14" width="15" height="15">
-<path d="M16 14H31V29H16V14Z" fill="#fff"/>
-</mask>
-<g mask="url(#mask0_2_405)">
-<path d="M29.75 20.9251V21.5001C29.7494 22.8478 29.3132 24.1593 28.5063 25.2389C27.6995 26.3184 26.5653 27.1083 25.2729 27.4906C23.9805 27.8729 22.5991 27.8272 21.3348 27.3603C20.0705 26.8934 18.991 26.0303 18.2573 24.8998C17.5236 23.7692 17.175 22.4318 17.2635 21.0869C17.352 19.7421 17.8729 18.4618 18.7484 17.4372C19.6239 16.4125 20.8072 15.6984 22.1218 15.4012C23.4364 15.104 24.8118 15.2397 26.043 15.7881" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M29.75 16.5011L26.625 19.6291L25.0625 21.1931L23.5 22.7571L21.625 20.8821" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-</g>
-</g>
-</g>
-</g>
-<defs>
-<filter id="filter0_d_2_405" x="0" y="0" width="47" height="47" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="2"/>
-<feGaussianBlur stdDeviation="4"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_405"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_405" result="shape"/>
-</filter>
-<linearGradient id="paint0_linear_2_405" x1="23.5" y1="6" x2="23.5" y2="37" gradientUnits="userSpaceOnUse">
-<stop stopColor="white" stopOpacity="0.3"/>
-<stop offset="1" stopColor="white" stopOpacity="0"/>
-</linearGradient>
-<clipPath id="clip0_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip1_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip2_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-</defs>
-</svg>
+  <img src="img/Icon-circle.png" alt="Years" className="feature-icon" />
     <span>Selective Bets Only</span>
   </div>
 
   <div className="feature-item">
-       <svg width="38" height="38" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_2_405)">
-<rect x="8" y="6" width="31" height="31" rx="15.5" fill="white" fillOpacity="0.14" shapeRendering="crispEdges"/>
-<rect x="8.5" y="6.5" width="30" height="30" rx="15" stroke="url(#paint0_linear_2_405)" shapeRendering="crispEdges"/>
-<g clipPath="url(#clip0_2_405)">
-<g clipPath="url(#clip1_2_405)">
-<g clipPath="url(#clip2_2_405)">
-<mask id="mask0_2_405" style={{ maskType: 'luminance' }} maskUnits="userSpaceOnUse" x="16" y="14" width="15" height="15">
-<path d="M16 14H31V29H16V14Z" fill="#fff"/>
-</mask>
-<g mask="url(#mask0_2_405)">
-<path d="M29.75 20.9251V21.5001C29.7494 22.8478 29.3132 24.1593 28.5063 25.2389C27.6995 26.3184 26.5653 27.1083 25.2729 27.4906C23.9805 27.8729 22.5991 27.8272 21.3348 27.3603C20.0705 26.8934 18.991 26.0303 18.2573 24.8998C17.5236 23.7692 17.175 22.4318 17.2635 21.0869C17.352 19.7421 17.8729 18.4618 18.7484 17.4372C19.6239 16.4125 20.8072 15.6984 22.1218 15.4012C23.4364 15.104 24.8118 15.2397 26.043 15.7881" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M29.75 16.5011L26.625 19.6291L25.0625 21.1931L23.5 22.7571L21.625 20.8821" stroke="#007AFF" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
-</g>
-</g>
-</g>
-</g>
-</g>
-<defs>
-<filter id="filter0_d_2_405" x="0" y="0" width="47" height="47" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-<feFlood floodOpacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="2"/>
-<feGaussianBlur stdDeviation="4"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2_405"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2_405" result="shape"/>
-</filter>
-<linearGradient id="paint0_linear_2_405" x1="23.5" y1="6" x2="23.5" y2="37" gradientUnits="userSpaceOnUse">
-<stop stopColor="white" stopOpacity="0.3"/>
-<stop offset="1" stopColor="white" stopOpacity="0"/>
-</linearGradient>
-<clipPath id="clip0_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip1_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-<clipPath id="clip2_2_405">
-<rect width="15" height="15" fill="white" transform="translate(16 14)"/>
-</clipPath>
-</defs>
-</svg>
+  <img src="img/Icon-circle.png" alt="Years" className="feature-icon" />
     <span>ROI Driven Strategy</span>
   </div>
 </div>
